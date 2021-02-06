b091ab577c240546659b83bcf7576b54161ae53c
conchboy <panchaocn@hotmail.com>
2021-02-06 20:59:18 +0800

    game cycle and timer

--- rp_07_游戏循环和游戏时钟.py
+++ rp_07_游戏循环和游戏时钟.py
@@ -1,5 +1,6 @@
 import pygame
 
+# 游戏的初始化
 pygame.init()
 
 # 创建游戏窗口 480 * 700
@@ -21,8 +22,22 @@ screen.blit(hero, (170, 300))
 # 可以在所有绘制工作完成之后统一完成image.update()方法
 pygame.display.update()
 
+# 设置游戏时钟
+clock = pygame.time.Clock()
+
+# 游戏循环 -->意味着游戏的正式开始
+i = 0
 while True:
-    pass
+    # 1. 设置刷新率(帧数 = 60)
+    # tick方法可以定义循环体内部的代码执行的频率
+    clock.tick(60)
+    print(i)
+    i += 1
+
+
+    # 2. 检测用户交互
+    # 3. 更新所有图像的位置
+    # 4. 更新屏幕显示
 
 
 pygame.quit()
