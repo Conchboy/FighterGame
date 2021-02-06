20caa0b48896f2e097e8290ef8079591c9bf2615
conchboy <panchaocn@hotmail.com>
2021-02-06 20:32:28 +0800

    add hero

--- rp_03_创建游戏窗口.py
+++ rp_03_创建游戏窗口.py
@@ -2,7 +2,10 @@ import pygame
 
 pygame.init()
 
-# 创建游戏窗口
-screen = pygame.display.set_mode()
+# 创建游戏窗口 480 * 700
+screen = pygame.display.set_mode((480, 700))
+
+while True:
+    pass
 
 pygame.quit()
