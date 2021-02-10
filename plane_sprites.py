00b41e4c43be470bbba0c08e42a70a97b4bedab3
conchboy <panchaocn@hotmail.com>
2021-02-10 15:56:45 +0800

    Plane Sprites

---
+++ plane_sprites.py
@@ -0,0 +1,20 @@
+import pygame
+
+class GameSprite(pygame.sprite.Sprite):
+    """飞机大战游戏精灵"""
+
+    def __init__(self, image_name, speed = 1):
+        # 调用父类的初始化方法
+        super().__init__()
+        # 定义对象的属性
+        self.image = pygame.image.load(image_name)
+        self.rect = self.image.get_rect()
+        self.speed = speed
+
+    def update(self, *args, **kwargs) -> None:
+        self.
+        pass
+
+
+
+
