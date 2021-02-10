9f66c70c898792ff1c7060d68435f73b2eff7a36
conchboy <panchaocn@hotmail.com>
2021-02-10 17:07:15 +0800

    Plane Sprites

--- plane_sprites.py
+++ plane_sprites.py
@@ -4,8 +4,9 @@ import pygame
 class GameSprite(pygame.sprite.Sprite):
     """飞机大战游戏精灵"""
 
-    def __init__(self, image_name, speed = 1):
+    def __init__(self, image_name, speed=1):
         # !!主动调用父类的初始化方法 super().__init__()
+        super().__init__()
         # 定义对象的属性
         self.image = pygame.image.load(image_name)
         self.rect = self.image.get_rect()
---
+++ rp_12_演练精灵.py
@@ -0,0 +1,78 @@
+import pygame
+
+from plane_sprites import *
+
+# 游戏的初始化
+pygame.init()
+
+# 创建游戏窗口 480 * 700
+screen = pygame.display.set_mode((480, 700))
+
+
+# 绘制背景图像
+# 1. 加载图像数据
+bg = pygame.image.load("./images/background.png")
+# 2. bilt绘制图像
+screen.blit(bg, (0, 0))
+# 3. update更新屏幕显示
+# pygame.display.update()
+
+# 绘制英雄图像
+hero = pygame.image.load("./images/me1.png")
+screen.blit(hero, (170, 500))
+
+# 可以在所有绘制工作完成之后统一完成image.update()方法
+pygame.display.update()
+
+# 设置游戏时钟
+clock = pygame.time.Clock()
+
+
+hero_rect = pygame.Rect(150, 300, 102, 126)
+
+# 创建敌机精灵
+# for i in range(10):
+enemy = GameSprite("./images/enemy1.png")
+enemy1 = GameSprite("./images/enemy2.png", 3)
+enemy.rect.x += 150
+enemy_group = pygame.sprite.Group(enemy, enemy1)
+
+
+# 创建敌机的精灵组
+
+
+
+# 游戏循环 -->意味着游戏的正式开始
+
+while True:
+    # 1. 设置刷新率(帧数 = 60)
+    # tick方法可以定义循环体内部的代码执行的频率
+    clock.tick(60)
+
+    # 捕获事件
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("游戏退出...")
+            # 卸载所有模块
+            pygame.quit()
+            exit()
+
+    # 2. 检测用户交互,
+    if hero_rect.y > -hero_rect.height:
+        hero_rect.y -= 2
+    else:
+        hero_rect.y = 700  # + hero_rect.y
+    # 3. 更新所有图像的位置, blit()
+    screen.blit(bg, (0, 0))
+    screen.blit(hero, hero_rect)
+
+    # 让精灵组调用两个方法
+    # update - 让组中的所有精灵更新位置
+    enemy_group.update()
+    # draw - 在screen上绘制所有的精灵
+    enemy_group.draw(screen)
+
+    # 4. 更新屏幕显示, pygame.image.update()
+    pygame.display.update()
+
+pygame.quit()
